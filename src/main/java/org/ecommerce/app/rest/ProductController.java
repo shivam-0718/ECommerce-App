52cea914f6c9199a7201a4bff0dcbf72fe8f3b48
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-28 16:34:24 +0530

    Implementing addProduct() in the Controller.

--- src/main/java/org/ecommerce/app/rest/ProductController.java
+++ src/main/java/org/ecommerce/app/rest/ProductController.java
@@ -5,11 +5,14 @@ import org.ecommerce.app.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestPart;
 import java.util.List;
 
 @RestController
@@ -30,5 +33,11 @@ public class ProductController {
     public ResponseEntity<Product> getProduct(@PathVariable("id") int id) {
         Product product = service.fetchProduct(id);
         return new ResponseEntity<Product>(product, HttpStatus.OK);
+    }
+
+    @PostMapping("/add-product")
+    public ResponseEntity<String> addProduct(@RequestPart Product product, @RequestPart MultipartFile image) {
+        String response = service.addProduct(product, image);
+        return new ResponseEntity<String>(response, HttpStatus.CREATED);
     }
 }
