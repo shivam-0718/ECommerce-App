6d2f6fa8d321279eff1300c20509d61382e0547d
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-28 16:33:10 +0530

    Add image fields to Product class for enhanced product representation

--- src/main/java/org/ecommerce/app/model/Product.java
+++ src/main/java/org/ecommerce/app/model/Product.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -31,4 +32,10 @@ public class Product {
 
     private boolean productAvailable;
     private int stockQuantity;
+    private String imageName;
+    private String imageType;
+
+    @Lob
+    private byte[] imageData;
+
 }
