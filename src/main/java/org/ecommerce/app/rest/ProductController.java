ad1234773ca376bd2c126de1bc9103d777be5d4a
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-28 12:27:04 +0530

    Added getProduct() to return list of Products.

--- src/main/java/org/ecommerce/app/rest/ProductController.java
+++ src/main/java/org/ecommerce/app/rest/ProductController.java
@@ -1,15 +1,22 @@
 package org.ecommerce.app.rest;
 
+import org.ecommerce.app.model.Product;
+import org.ecommerce.app.service.IProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
+    @Autowired
+    private IProductService service;
+
     @GetMapping("/products")
-    public String getProduct() {
-        return "All Products";
+    public List<Product> getProduct() {
+        return List.of();
     }
 }
