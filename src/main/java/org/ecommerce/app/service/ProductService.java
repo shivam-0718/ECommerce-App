5640c88c13348154148d602d5b5f70cccf083e46
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-28 13:48:00 +0530

    Add fetchProduct method to retrieve a product by ID

--- src/main/java/org/ecommerce/app/service/ProductService.java
+++ src/main/java/org/ecommerce/app/service/ProductService.java
@@ -1,10 +1,12 @@
 package org.ecommerce.app.service;
 
+import org.ecommerce.app.exception.ProductNotFoundException;
 import org.ecommerce.app.model.Product;
 import org.ecommerce.app.repo.IProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService implements IProductService{
@@ -15,5 +17,14 @@ public class ProductService implements IProductService{
     @Override
     public List<Product> getAllProducts() {
         return repo.findAll();
+    }
+
+    @Override
+    public Product fetchProduct(int id) {
+        Optional<Product> optional = repo.findById(id);
+        if(optional.isPresent()){
+            return optional.get();
+        }
+        throw new ProductNotFoundException("Product with given id is not available. Please try again with the correct id.");
     }
 }
