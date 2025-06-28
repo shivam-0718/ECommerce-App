2546b905294a8e545eb0fcb1d808145bb1ea0998
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-28 13:47:32 +0530

    Add JsonFormat annotation to releaseDate for consistent date formatting

--- src/main/java/org/ecommerce/app/model/Product.java
+++ src/main/java/org/ecommerce/app/model/Product.java
@@ -1,5 +1,6 @@
 package org.ecommerce.app.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,12 +19,16 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String name;
     private String description;
     private String brand;
     private BigDecimal price;
     private String category;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
+
     private boolean productAvailable;
     private int stockQuantity;
 }
