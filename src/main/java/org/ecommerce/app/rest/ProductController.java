bb08646551b71c9ad0fa8136e7aac40be75269df
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-06-28 17:11:05 +0530

    Add updateProduct() for product updates

--- src/main/java/org/ecommerce/app/rest/ProductController.java
+++ src/main/java/org/ecommerce/app/rest/ProductController.java
@@ -5,14 +5,15 @@ import org.ecommerce.app.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
@@ -46,5 +47,11 @@ public class ProductController {
         Product product = service.fetchProduct(id);
         byte[] imageData = product.getImageData();
         return new ResponseEntity<byte[]>(imageData, HttpStatus.OK);
+    }
+
+    @PutMapping("/update-product")
+    public ResponseEntity<String> updateProduct(@RequestPart Product product, @RequestPart MultipartFile image) {
+        String response = service.updateProduct(product, image);
+        return new ResponseEntity<String>(response, HttpStatus.OK);
     }
 }
